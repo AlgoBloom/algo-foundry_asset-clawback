db2fe921509e7c804af1820af3baad8855ecd971
Joshua Bloom <bloomblockchain@gmail.com>
2022-11-12 08:19:44 -0500

    Added submit to network function and created a txn var to hold the object that is returned after sending a signed raw transaction to the blockchain.

--- create_nft.js
+++ create_nft.js
@@ -1,4 +1,14 @@
+// importing algosdk
 const algosdk = require("algosdk");
 
+// using algosdk to create a algod client instance
 const algodClient = new algosdk.Algodv2(process.env.ALGOD_TOKEN, process.env.ALGOD_SERVER, process.env.ALGOD_PORT);
 
+// secret key of creator
+const creatorSecretKey = algosdk.mnemonicToSecretKey(process.env.MNEMONIC_CREATOR);
+
+// this function submits the txn to the network
+const submitToNetwork = async (signedTxn) => {
+    // returns txn object
+    let txn = await algodClient.sendRawTransaction(signedTxn).do();
+}
